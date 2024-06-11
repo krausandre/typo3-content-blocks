4aec7664dac9d010c4b463e11cc02d8f019dddce
Andre Kraus <info@andrekraus.com>
2024-06-11 11:22:59 +0200

    [TASK][WIP] Basic structure for editor

---
+++ typo3/sysext/make/Resources/Public/JavaScript/editor/content-block-editor-right-pain.js
@@ -0,0 +1,16 @@
+/*
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+var __decorate=function(t,e,o,r){var n,i=arguments.length,c=i<3?e:null===r?r=Object.getOwnPropertyDescriptor(e,o):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)c=Reflect.decorate(t,e,o,r);else for(var l=t.length-1;l>=0;l--)(n=t[l])&&(c=(i<3?n(c):i>3?n(e,o,c):n(e,o))||c);return i>3&&c&&Object.defineProperty(e,o,c),c};import{html,LitElement}from"lit";import{customElement,property}from"lit/decorators.js";import"@typo3/backend/element/icon-element.js";let ContentBlockEditorRightPain=class extends LitElement{render(){return html`
+      <p>I am the Right Pain</p>
+
+    `}createRenderRoot(){return this}};__decorate([property()],ContentBlockEditorRightPain.prototype,"name",void 0),ContentBlockEditorRightPain=__decorate([customElement("content-block-editor-right-pain")],ContentBlockEditorRightPain);export{ContentBlockEditorRightPain};
