cf8ecc23ed28b89d9c5b1ec63ef5222f891c8b02
Manuel Schnabel <mschnabel1992@gmail.com>
2024-06-14 11:35:14 +0200

    [WIP] edit input values in right pane

--- typo3/sysext/make/Resources/Public/JavaScript/content-blocks/editor/right-pane.js
+++ typo3/sysext/make/Resources/Public/JavaScript/content-blocks/editor/right-pane.js
@@ -10,14 +10,14 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-var __decorate=function(e,t,o,r){var l,a=arguments.length,n=a<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,o):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)n=Reflect.decorate(e,t,o,r);else for(var i=e.length-1;i>=0;i--)(l=e[i])&&(n=(a<3?l(n):a>3?l(t,o,n):l(t,o))||n);return a>3&&n&&Object.defineProperty(t,o,n),n};import{html,LitElement}from"lit";import{customElement,property}from"lit/decorators.js";import"@typo3/backend/element/icon-element.js";let ContentBlockEditorRightPane=class extends LitElement{render(){return console.log("Render right pane"),this.schema?html`
+var __decorate=function(e,t,o,r){var l,a=arguments.length,n=a<3?t:null===r?r=Object.getOwnPropertyDescriptor(t,o):r;if("object"==typeof Reflect&&"function"==typeof Reflect.decorate)n=Reflect.decorate(e,t,o,r);else for(var i=e.length-1;i>=0;i--)(l=e[i])&&(n=(a<3?l(n):a>3?l(t,o,n):l(t,o))||n);return a>3&&n&&Object.defineProperty(t,o,n),n};import{html,LitElement}from"lit";import{customElement,property}from"lit/decorators.js";import"@typo3/backend/element/icon-element.js";import{live}from"lit/directives/live.js";let ContentBlockEditorRightPane=class extends LitElement{render(){return console.log("Render right pane"),console.log(this.values),this.schema?html`
         ${this.schema.properties.map((e=>html` ${this.renderFormFieldset(e)}`))}
       `:html`No field was selected`}renderFormFieldset(e){return html`
       <div class="form-group">
         ${"boolean"===e.dataType?this.renderFormField(e):""}
         <label for="${e.name}" class="${"boolean"===e.dataType?"form-check-label fw-bold":"form-label"}">Property '${e.name}'</label>
         ${"boolean"!==e.dataType?this.renderFormField(e):""}
-      </div>`}renderFormField(e){switch(e.dataType){case"text":return html`<input @blur="${this.dispatchBlurEvent}" type="text" id="${e.name}" value="${this.values[e.name]||e.default||""}" class="form-control" />`;case"number":return html`<input @blur="${this.dispatchBlurEvent}" type="number" id="${e.name}" value="${this.values[e.name]||e.default}" class="form-control" />`;case"select":return html`<select @blur="${this.dispatchBlurEvent}" class="form-control" id="${e.name}" >
+      </div>`}renderFormField(e){switch(e.dataType){case"text":let t=e.default||"";return void 0!==this.values[e.name]&&(t=this.values[e.name],console.log(e.name+":"+t)),html`<input @blur="${this.dispatchBlurEvent}" type="text" id="${e.name}" .value="${live(t)}" class="form-control" />`;case"number":return html`<input @blur="${this.dispatchBlurEvent}" type="number" id="${e.name}" value="${this.values[e.name]||e.default}" class="form-control" />`;case"select":return html`<select @blur="${this.dispatchBlurEvent}" class="form-control" id="${e.name}" >
           <option value="">Choose...</option>
           ${e.items.map((e=>html`
             <option value="${e.value}">${e.label}</option>`))}
